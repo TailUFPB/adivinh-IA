73de76f849361aff2bb1b61d21ac47531005b76f
Cândido Alfredo Carvalho de Lucena Filho <candidoalfilho@hotmail.com>
2021-11-04 18:38:22 -0300

    Regras

--- src/pages/Rules/index.js
+++ src/pages/Rules/index.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './index.css'
 
 function Rules() {
     return (
@@ -8,6 +9,23 @@ function Rules() {
                     Adivinh-IA
                 </h1>
             </Link>
+            <div className="container">
+                <div className="titleRule">Regras</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper vel justo eu lacinia. Curabitur gravida, lorem at consectetur ultricies, est eros tempor sapien, sed dignissim orci lacus eu metus. Aenean molestie augue at augue vehicula lacinia. Aliquam malesuada auctor elit vel feugiat. Nam nec augue sollicitudin, sodales mauris ac, semper diam. Aenean efficitur dignissim ipsum quis dapibus. Donec suscipit, mauris eget cursus finibus, neque justo finibus justo, non vehicula risus lectus ut enim. Donec sed justo euismod, vehicula massa non, posuere ante. Duis a scelerisque urna, ut ultrices enim. Donec erat dui, gravida sodales ultrices a, consectetur eu nulla. Duis imperdiet lobortis congue.</p>
+                <ol>
+                    <li>
+
+                    </li>
+                    <li>
+
+                    </li>
+                    <li>
+
+                    </li>
+                </ol>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper vel justo eu lacinia. Curabitur gravida, lorem at consectetur ultricies, est eros tempor sapien, sed dignissim orci lacus eu metus. Aenean molestie augue at augue vehicula lacinia. Aliquam malesuada auctor elit vel feugiat. Nam nec augue sollicitudin, sodales mauris ac, semper diam. Aenean efficitur dignissim ipsum quis dapibus. Donec suscipit, mauris eget cursus finibus, neque justo finibus justo, non vehicula risus lectus ut enim. Donec sed justo euismod, vehicula massa non, posuere ante. Duis a scelerisque urna, ut ultrices enim. Donec erat dui, gravida sodales ultrices a, consectetur eu nulla. Duis imperdiet lobortis congue.</p>
+                
+            </div>
         </div>
     )
 }
