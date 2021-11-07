09a19b498a8560edc9cb555b19c8852099f29905
Cândido Alfredo Carvalho de Lucena Filho <candidoalfilho@hotmail.com>
2021-11-07 17:40:11 -0300

    Acabando a página de About

--- src/pages/About/cards.js
+++ src/pages/About/cards.js
@@ -1,6 +1,7 @@
 import CardItem from './cardItem';
 import './cards.css';
 
+
 function Cards() {
     return (
         <div className = 'cards'>
@@ -8,13 +9,13 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem 
-                        src = "https://avatars.githubusercontent.com/u/26287741?v=4"
+                        src = {require("../../assets/jorge.jpg").default}
                         text = "Jorge Gomes"
                         label='Ciência da Computação'
                         path='https://www.linkedin.com/in/jorge-gomes-578205167/'
                         />
                         <CardItem 
-                        src = "https://media-exp1.licdn.com/dms/image/C4D03AQEsqKdzs424pQ/profile-displayphoto-shrink_200_200/0/1600348779639?e=1640822400&v=beta&t=SQIOoxw5TUa6hCpS9lOsTHrukp6egZYJ-E0BKIG-_po"
+                        src = {require("../../assets/epitacio.jpg").default}
                         text = "Epitácio Neto"
                         label = "Engenharia da Computação"
                         path='https://www.linkedin.com/in/epitacio-neto-61ba98168/'
@@ -23,7 +24,7 @@ function Cards() {
                     </ul>
                     <ul className="cards__items">
                     <CardItem
-                    src='https://avatars.githubusercontent.com/u/40697115?v=4'
+                    src={require("../../assets/yann.jpg").default}
                     text='Yann Nóbrega'
                     label='Medicina'
                     path='https://www.linkedin.com/in/yannngn/'
@@ -35,25 +36,25 @@ function Cards() {
                     path='https://www.linkedin.com/in/candidoalfredocl/'
                     />
                     <CardItem
-                    src=''
+                    src={require("../../assets/arthur.jpg").default}
                     text='Arthur Silva'
-                    label='Curso'
-                    path=''
+                    label='Ciência da Computação'
+                    path='https://www.linkedin.com/in/arthur-s-cavalcante-ferreira/'
                     />
                     
                     </ul>
                     <ul className="cards__items">
                         <CardItem
-                    src='https://media-exp1.licdn.com/dms/image/C5603AQG0qag0TiS5Ew/profile-displayphoto-shrink_200_200/0/1571352848487?e=1640822400&v=beta&t=YowmbKPTeK3rsH2dIOdzjlLuHQo0SfjgDNf7NDL6cfM'
+                    src= {require("../../assets/daniel.jpg").default}
                     text='Daniel Cabral'
                     label='Engenharia Civil'
                     path='https://www.linkedin.com/in/danielcabralcosta/'
                     />
                     <CardItem
-                    src=''
+                    src={require("../../assets/humberto.jpg").default}
                     text='Humberto Escorel'
-                    label='Curso'
-                    path=''
+                    label='Engenharia da Computação'
+                    path='https://www.linkedin.com/in/humberto-escorel-154641127/'
                     />
                     </ul>
                 </div>
